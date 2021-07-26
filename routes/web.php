69e3350acc4b66d71d7ccee1364de2175c3ecd7e
Sr Full Stack Dev Muhammad Tariq <tariq.zameen.com@gmail.com>
2021-07-26 15:21:32 +0500

    first init commit

---
+++ routes/web.php
@@ -0,0 +1,82 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get( '/' , function () 
+                            {
+                                echo "Laravel First Lecture about Route";
+                            } 
+);
+
+Route::get("test",function(){ echo "Test Route";});
+
+
+//One Route Parameters
+Route::get("student/{student_id}",function($std_id){ 
+
+    echo "Delete Student Record # " . $std_id;
+
+});
+
+//Two Route Parameters
+Route::get("student/{student_id}/{name}",function($std_id,$name){ 
+
+    echo "Delete Student Record # " . $std_id . " Name : " . $name;
+
+});
+
+
+//Optional Route Parameters
+//when you will put parameter as optional then you have to give default value to that // functional parametee
+
+Route::get("get_book/{id?}",function($id=NULL){ 
+
+    echo "Book Record " . $id;
+
+});
+
+
+Route::get("emp/{id}/dept/{name}/working",function($id,$name){
+    echo "$id :: $name";
+});
+
+
+//Route Parameters Validation
+
+Route::get("std/{id}",function($id){
+
+    echo "$id";
+
+})->where(["id" => "[0-9]{4}"]);
+
+Route::get("std_name/{name}",function($name){
+
+    echo "$name";
+
+})->where(["name" => "[a-zA-Z]{4}"]);
+
+
+Route::get("name/{name}",function($name){
+
+    echo "$name";
+
+})->where(["name" => "[a-zA-Z]+"]);
+
+//Assignements
+//Phone number validation e.g 0303-3445235
+//CNIC validation e.g 35202-0340607-8
+//Email validation e.g asdsa@gmail.com
+//IP validation e.g 255.255.255.255
+
+
